d14b19f2f7a38abdacb69f7c1b0e7ad06a7b16bf
Eric <2635863525@qq.com>
2019-02-22 10:33:08 +0800

    完成APKTool类的初始化

---
+++ src/com/xc/tools/com/xc/tools/decode/APKTool.java
@@ -0,0 +1,18 @@
+package com.xc.tools.com.xc.tools.decode;
+
+
+/**
+ * 调用APKTool对APK文件进行反编译的工具类
+ */
+public class APKTool {
+    /**
+     *
+     * @param src 文件来源
+     * @param dest 文件存放地址
+     */
+    public  static void decode(String src,String dest){
+
+
+
+    }
+}
