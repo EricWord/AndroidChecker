8179069bceb5523f85242bf6359574e5e10b5a3e
tom_gl <xiaocui@163.com>
2019-02-23 22:54:47 +0800

    图形化界面

---
+++ src/com/xc/ui/MainPage.java
@@ -0,0 +1,184 @@
+package com.xc.ui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * Android应用检测工具主界面
+ */
+public class MainPage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //设置标题
+        stage.setTitle("Android应用检测工具");
+        //设置宽度
+        stage.setWidth(1000);
+        //设置高度
+        stage.setHeight(700);
+        BorderPane border = new BorderPane();
+        HBox hbox = addHBox();
+        border.setTop(hbox);
+        border.setLeft(addVBox());
+        addStackPane(hbox);         // Add stack to HBox in top region
+
+        border.setCenter(addGridPane());
+        border.setRight(addFlowPane());
+        Scene scene = new Scene(border);
+        stage.setScene(scene);
+
+
+        //显示
+        stage.show();
+
+
+    }
+
+    public HBox addHBox() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Button buttonCurrent = new Button("Current");
+        buttonCurrent.setPrefSize(100, 20);
+
+        Button buttonProjected = new Button("Projected");
+        buttonProjected.setPrefSize(100, 20);
+        hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+
+        return hbox;
+    }
+
+    public VBox addVBox() {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+
+        Text title = new Text("Data");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        vbox.getChildren().add(title);
+
+        Hyperlink options[] = new Hyperlink[]{
+                new Hyperlink("Sales"),
+                new Hyperlink("Marketing"),
+                new Hyperlink("Distribution"),
+                new Hyperlink("Costs")};
+
+        for (int i = 0; i < 4; i++) {
+            VBox.setMargin(options[i], new Insets(0, 0, 0, 8));
+            vbox.getChildren().add(options[i]);
+        }
+
+        return vbox;
+    }
+
+    public void addStackPane(HBox hb) {
+        StackPane stack = new StackPane();
+        Rectangle helpIcon = new Rectangle(30.0, 25.0);
+        helpIcon.setFill(new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE,
+                new Stop[]{
+                        new Stop(0, Color.web("#4977A3")),
+                        new Stop(0.5, Color.web("#B0C6DA")),
+                        new Stop(1, Color.web("#9CB6CF")),}));
+        helpIcon.setStroke(Color.web("#D0E6FA"));
+        helpIcon.setArcHeight(3.5);
+        helpIcon.setArcWidth(3.5);
+
+        Text helpText = new Text("?");
+        helpText.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+        helpText.setFill(Color.WHITE);
+        helpText.setStroke(Color.web("#7080A0"));
+
+        stack.getChildren().addAll(helpIcon, helpText);
+        stack.setAlignment(Pos.CENTER_RIGHT);     // Right-justify nodes in stack
+        StackPane.setMargin(helpText, new Insets(0, 10, 0, 0)); // Center "?"
+
+        hb.getChildren().add(stack);            // Add to HBox from Example 1-2
+        HBox.setHgrow(stack, Priority.ALWAYS);    // Give stack any extra space
+    }
+
+    public GridPane addGridPane() {
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(0, 10, 0, 10));
+
+        // Category in column 2, row 1
+        Text category = new Text("Sales:");
+        category.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        grid.add(category, 1, 0);
+
+        // Title in column 3, row 1
+        Text chartTitle = new Text("Current Year");
+        chartTitle.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        grid.add(chartTitle, 2, 0);
+
+        // Subtitle in columns 2-3, row 2
+        Text chartSubtitle = new Text("Goods and Services");
+        grid.add(chartSubtitle, 1, 1, 2, 1);
+
+        // House icon in column 1, rows 1-2
+        ImageView imageHouse = new ImageView(
+                new Image(MainPage.class.getResourceAsStream("/resources/images/1.jpg")));
+        grid.add(imageHouse, 0, 0, 1, 2);
+
+        // Left label in column 1 (bottom), row 3
+        Text goodsPercent = new Text("Goods\n80%");
+        GridPane.setValignment(goodsPercent, VPos.BOTTOM);
+        grid.add(goodsPercent, 0, 2);
+
+        // Chart in columns 2-3, row 3
+        ImageView imageChart = new ImageView(
+                new Image(MainPage.class.getResourceAsStream("/resources/images/2.jpg")));
+        grid.add(imageChart, 1, 2, 2, 1);
+
+        // Right label in column 4 (top), row 3
+        Text servicesPercent = new Text("Services\n20%");
+        GridPane.setValignment(servicesPercent, VPos.TOP);
+        grid.add(servicesPercent, 3, 2);
+
+        return grid;
+    }
+
+    public FlowPane addFlowPane() {
+        FlowPane flow = new FlowPane();
+        flow.setPadding(new Insets(5, 0, 5, 0));
+        flow.setVgap(4);
+        flow.setHgap(4);
+        flow.setPrefWrapLength(170); // preferred width allows for two columns
+        flow.setStyle("-fx-background-color: DAE6F3;");
+
+        ImageView pages[] = new ImageView[8];
+        for (int i = 0; i < 8; i++) {
+            pages[i] = new ImageView(
+                    new Image(MainPage.class.getResourceAsStream(
+                            "/resources/images/chart" + (i + 1) + ".jpg")));
+            flow.getChildren().add(pages[i]);
+        }
+
+        return flow;
+    }
+}
